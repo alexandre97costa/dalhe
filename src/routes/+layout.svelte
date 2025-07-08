0308e1cbe067a0db90cfbe8871c1fb77d68545ea
Alexandre Costa <alexandre97costa@gmail.com>
2025-07-08 21:01:57 +0100

    nav & light switch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
 	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
+	import Nav from '$lib/components/Nav.svelte';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<ModeWatcher />
+<Nav />
+
+{@render children?.()}
