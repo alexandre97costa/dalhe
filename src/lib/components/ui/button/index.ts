9c89683fc8eb7dc53a4bb8a74c30a54e18ee85a5
Alexandre Costa <alexandre97costa@gmail.com>
2025-10-21 23:49:24 +0100

    added components & copilot instructions

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -6,6 +6,9 @@ import Root, {
 } from "./button.svelte";
 
 export {
+	Root,
+	type ButtonProps as Props,
+	//
 	Root as Button,
 	buttonVariants,
 	type ButtonProps,
--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -2,34 +2,44 @@
 	import { title } from '$lib/store.js';
 	import { m } from '$lib/paraglide/messages.js';
 
+	import { Label } from '$lib/components/ui/label/index.js';
 	import * as InputOTP from '$lib/components/ui/input-otp/index.js';
 	import { REGEXP_ONLY_DIGITS } from 'bits-ui';
 
 	title.set(m.nav_add());
+
+	const id = $props.id();
 </script>
 
-<InputOTP.Root
-	maxlength={7}
-	pattern={REGEXP_ONLY_DIGITS}
-	on:change={(e) => console.log(e.detail.value)}
->
-	{#snippet children({ cells })}
-		<InputOTP.Group>
-			{#each cells.slice(0, 2) as cell}
-				<InputOTP.Slot {cell} />
-			{/each}
-		</InputOTP.Group>
-		<InputOTP.Separator>:</InputOTP.Separator>
-		<InputOTP.Group>
-			{#each cells.slice(2, 4) as cell}
-				<InputOTP.Slot {cell} />
-			{/each}
-		</InputOTP.Group>
-		<InputOTP.Separator>.</InputOTP.Separator>
-		<InputOTP.Group>
-			{#each cells.slice(4, 7) as cell}
-				<InputOTP.Slot {cell} />
-			{/each}
-		</InputOTP.Group>
-	{/snippet}
-</InputOTP.Root>
+
+
+<div class="flex w-full max-w-sm flex-col gap-3">
+	<Label for="input-laptime-{id}">Tempo</Label>
+
+	<InputOTP.Root
+		maxlength={7}
+		pattern={REGEXP_ONLY_DIGITS}
+		on:change={(e) => console.log(e)}
+		id="input-laptime-{id}"
+	>
+		{#snippet children({ cells })}
+			<InputOTP.Group>
+				{#each cells.slice(0, 2) as cell}
+					<InputOTP.Slot {cell} />
+				{/each}
+			</InputOTP.Group>
+			<InputOTP.Separator>:</InputOTP.Separator>
+			<InputOTP.Group>
+				{#each cells.slice(2, 4) as cell}
+					<InputOTP.Slot {cell} />
+				{/each}
+			</InputOTP.Group>
+			<InputOTP.Separator>.</InputOTP.Separator>
+			<InputOTP.Group>
+				{#each cells.slice(4, 7) as cell}
+					<InputOTP.Slot {cell} />
+				{/each}
+			</InputOTP.Group>
+		{/snippet}
+	</InputOTP.Root>
+</div>
