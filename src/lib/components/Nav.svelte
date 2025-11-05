39181585c7f94f78286edf7c3f7343a3154f1fe8
Alexandre Costa <alexandre97costa@gmail.com>
2025-11-05 22:16:34 +0000

    icones na nav

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,63 +1,81 @@
-<script>
+<script lang="ts">
 	import * as NavigationMenu from '$lib/components/ui/navigation-menu/index.js';
 	import { Button } from '$lib/components/ui/button/index';
-	import IconPlus from '@lucide/svelte/icons/plus';
 	import NavigationMenuItem from './ui/navigation-menu/navigation-menu-item.svelte';
 	import { navigationMenuTriggerStyle } from '$lib/components/ui/navigation-menu/navigation-menu-trigger.svelte';
-	import { Badge } from '$lib/components/ui/badge/index.js';
 	import { cn } from '$lib/utils.js';
-	import CircleHelpIcon from '@lucide/svelte/icons/circle-help';
-	import CircleIcon from '@lucide/svelte/icons/circle';
-	import CircleCheckIcon from '@lucide/svelte/icons/circle-check';
-	import IconClockPlus from '@lucide/svelte/icons/clock-plus';
-
+	import { type Icon as IconType, Home, Trophy, Flag, UserRound, Plus } from '@lucide/svelte';
 	import { m } from '$lib/paraglide/messages.js';
 
 	let { open = $bindable(false) } = $props();
 
-	const links = [
+	type MenuItem = {
+		title: string;
+		href: string;
+		description: string;
+		icon: typeof IconType;
+		order: number;
+	};
+
+	const menuItems: MenuItem[] = [
 		{
 			title: m.nav_home(),
 			href: '/demo/aura-mockup',
 			description:
-				'A modal dialog that interrupts the user with important content and expects a response.'
+				'A modal dialog that interrupts the user with important content and expects a response.',
+			icon: Home,
+			order: 1
 		},
 		{
 			title: m.nav_leaders(),
 			href: '/demo/supa',
-			description: 'For sighted users to preview content available behind a link.'
+			description: 'For sighted users to preview content available behind a link.',
+			icon: Trophy,
+			order: 2
 		},
 		{
 			title: m.nav_tournaments(),
 			href: '/tournaments',
 			description:
-				'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.'
+				'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
+			icon: Flag,
+			order: 4
 		},
 		{
 			title: m.nav_profile(),
 			href: '/profile',
 			description:
-				'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.'
+				'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
+			icon: UserRound,
+			order: 5
 		}
 	];
 </script>
 
+{#snippet menuItem(item: MenuItem, i: number)}
+	{@const Icon = item.icon}
+	<NavigationMenu.Item
+		class={cn('col-span-2 flex justify-center', i < 2 ? 'order-first' : 'order-last')}
+	>
+		<NavigationMenu.Link href={item.href} class="flex flex-col items-center gap-2">
+			<Icon strokeWidth=2.5 />
+			<span class="text-sm font-medium">
+				{item.title}
+			</span>
+		</NavigationMenu.Link>
+	</NavigationMenu.Item>
+{/snippet}
+
 <NavigationMenu.Root
 	viewport={false}
-	class="bg-popover/20 dark:border-input max-w-full border-t-1 px-4 py-4 shadow-xl backdrop-blur-sm "
+	class="bg-popover/20 dark:border-input max-w-full border-t px-4 py-2 shadow-xl backdrop-blur-sm "
 >
-	<NavigationMenu.List class="grid min-w-[100vw] grid-cols-10 gap-3 md:min-w-full">
-		{#each links as link, i (i)}
-			{#if i == 0 || i == 1}
-				<NavigationMenu.Item class="col-span-2 flex justify-center">
-					<NavigationMenu.Link href={link.href}>
-						{link.title}
-					</NavigationMenu.Link>
-				</NavigationMenu.Item>
-			{/if}
+	<NavigationMenu.List class="grid min-w-screen grid-cols-10 gap-3 md:min-w-full">
+		{#each menuItems as item, i}
+			{@render menuItem(item, i)}
 		{/each}
 
-		<NavigationMenuItem class="relative col-span-2  flex h-4 justify-center">
+		<NavigationMenuItem class="relative col-span-2 flex h-3 justify-center ">
 			<NavigationMenu.Link asChild>
 				{#snippet child()}
 					<Button
@@ -65,42 +83,33 @@
 						variant=""
 						class="
 								absolute
-								bottom-1 
-								mx-2.5
+								bottom-0.5 
 								aspect-square
-								scale-125
 								cursor-pointer
+								h-13 w-13
 
 								rounded-4xl
-								border-1
+								border-[1.5px]
 								border-purple-300 
-								bg-purple-400/20 
+								bg-purple-400/20
 
-								py-5
 								text-purple-600 
 								shadow-lg
 								shadow-purple-500/20
-								backdrop-blur-sm
+								backdrop-blur-lg
 								dark:border-purple-400
 								dark:text-purple-400 
 								dark:shadow-purple-400/20  
 							"
 					>
-						<IconPlus size="48" />
+					<div class="scale-[180%]">
+
+						<Plus  strokeWidth=2 />
+					</div>
 						<span class="sr-only">{m.nav_add}</span>
 					</Button>
 				{/snippet}
 			</NavigationMenu.Link>
 		</NavigationMenuItem>
-
-		{#each links as link, i (i)}
-			{#if i == 2 || i == 3}
-				<NavigationMenu.Item class="col-span-2 flex justify-center">
-					<NavigationMenu.Link href={link.href}>
-						{link.title}
-					</NavigationMenu.Link>
-				</NavigationMenu.Item>
-			{/if}
-		{/each}
 	</NavigationMenu.List>
 </NavigationMenu.Root>
