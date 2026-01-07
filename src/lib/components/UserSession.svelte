6ec1f6010b5b8ca142d5235bdde68e6c8cb6a4e4
Alexandre Costa <alexandre97costa@gmail.com>
2026-01-07 22:50:35 +0000

    login ui cont

--- src/lib/components/UserSession.svelte
+++ src/lib/components/UserSession.svelte
@@ -5,13 +5,17 @@
 	import Button from './ui/button/button.svelte';
 	import { buttonVariants } from '$lib/components/ui/button/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
 
 	import UserIcon from '@lucide/svelte/icons/user';
+	import ChevronDownIcon from '@lucide/svelte/icons/chevron-down';
+	import ChevronUpIcon from '@lucide/svelte/icons/chevron-up';
 	import { m } from '$lib/paraglide/messages.js';
 	import { toast } from 'svelte-sonner';
 
 	let sessionData = $state<Session | null>(null);
 	let testing = $state(false);
+	let open = $state(false); // for dropdown menu
 
 	onMount(async () => {
 		const {
@@ -29,65 +33,43 @@
 {#if !sessionData && !testing}
 	<!-- TODO: mudar o que este botao faz quando tivermos a user session pronta -->
 	<!-- <Button variant="cta" size="sm" onclick={() => supabase.auth.signInWithOAuth({ provider: 'google' })}> -->
-	<Button variant="cta" size="sm" onclick={() => {testing = true}}>
+	<Button
+		variant="cta"
+		size="sm"
+		onclick={() => {
+			testing = true;
+		}}
+	>
 		{m.login_button()}
 	</Button>
 {:else}
-	<DropdownMenu.Root>
+	<DropdownMenu.Root bind:open>
 		<DropdownMenu.Trigger>
 			{#snippet child({ props })}
-				<Button {...props} variant="outline" >
-					<UserIcon class="mr-2 h-4 w-4" />
-					<span>{sessionData?.user.email ?? 'Test User'}</span>
+				<Button {...props} variant="outline" class="dark:bg-background  cursor-pointer ">
+					<!-- <span>{sessionData?.user.email ?? ''}</span> -->
+					<Avatar.Root class=" scale-125 border-2 border-purple-500 dark:border-purple-600">
+						<Avatar.Image
+							src="https://avatars.githubusercontent.com/u/38655930?v=4"
+							alt="@shadcn"
+						/>
+						<Avatar.Fallback>CN</Avatar.Fallback>
+					</Avatar.Root>
+					<ChevronDownIcon class={'ml-2 h-4 w-4 duration-200 ' + (open ? '-scale-100' : '')} />
 				</Button>
 			{/snippet}
 		</DropdownMenu.Trigger>
+
 		<DropdownMenu.Content class="w-56" align="start">
 			<DropdownMenu.Label>My Account</DropdownMenu.Label>
-			<DropdownMenu.Group>
-				<DropdownMenu.Item>
-					Profile
-					<DropdownMenu.Shortcut>⇧⌘P</DropdownMenu.Shortcut>
-				</DropdownMenu.Item>
-				<DropdownMenu.Item>
-					Billing
-					<DropdownMenu.Shortcut>⌘B</DropdownMenu.Shortcut>
-				</DropdownMenu.Item>
-				<DropdownMenu.Item>
-					Settings
-					<DropdownMenu.Shortcut>⌘S</DropdownMenu.Shortcut>
-				</DropdownMenu.Item>
-				<DropdownMenu.Item>
-					Keyboard shortcuts
-					<DropdownMenu.Shortcut>⌘K</DropdownMenu.Shortcut>
-				</DropdownMenu.Item>
-			</DropdownMenu.Group>
+			<DropdownMenu.Item>Profile</DropdownMenu.Item>
+			<DropdownMenu.Item>Settings</DropdownMenu.Item>
 			<DropdownMenu.Separator />
-			<DropdownMenu.Group>
-				<DropdownMenu.Item>Team</DropdownMenu.Item>
-				<DropdownMenu.Sub>
-					<DropdownMenu.SubTrigger>Invite users</DropdownMenu.SubTrigger>
-					<DropdownMenu.SubContent>
-						<DropdownMenu.Item>Email</DropdownMenu.Item>
-						<DropdownMenu.Item>Message</DropdownMenu.Item>
-						<DropdownMenu.Separator />
-						<DropdownMenu.Item>More...</DropdownMenu.Item>
-					</DropdownMenu.SubContent>
-				</DropdownMenu.Sub>
-				<DropdownMenu.Item>
-					New Team
-					<DropdownMenu.Shortcut>⌘+T</DropdownMenu.Shortcut>
-				</DropdownMenu.Item>
-			</DropdownMenu.Group>
+			<DropdownMenu.Label>Preferences</DropdownMenu.Label>
+			<DropdownMenu.Item>Theme</DropdownMenu.Item>
+			<DropdownMenu.Item>Language</DropdownMenu.Item>
 			<DropdownMenu.Separator />
-			<DropdownMenu.Item>GitHub</DropdownMenu.Item>
-			<DropdownMenu.Item>Support</DropdownMenu.Item>
-			<DropdownMenu.Item disabled>API</DropdownMenu.Item>
-			<DropdownMenu.Separator />
-			<DropdownMenu.Item>
-				Log out
-				<DropdownMenu.Shortcut>⇧⌘Q</DropdownMenu.Shortcut>
-			</DropdownMenu.Item>
+			<DropdownMenu.Item>Log out</DropdownMenu.Item>
 		</DropdownMenu.Content>
 	</DropdownMenu.Root>
 {/if}
