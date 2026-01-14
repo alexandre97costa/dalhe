c5e60162969ad02ba80f54c9bec31e320357c5f9
Alexandre Costa <alexandre97costa@gmail.com>
2026-01-14 02:12:40 +0000

    checkpoint - session works

--- src/routes/auth/confirm/+server.ts
+++ src/routes/auth/confirm/+server.ts
@@ -4,29 +4,32 @@ import { redirect } from '@sveltejs/kit'
 import type { RequestHandler } from './$types'
 
 export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
-  const token_hash = url.searchParams.get('token_hash')
-  const type = url.searchParams.get('type') as EmailOtpType | null
-  const next = url.searchParams.get('next') ?? '/demo/supa'
+	const token_hash = url.searchParams.get('token_hash')
+	const type = url.searchParams.get('type') as EmailOtpType | null
+	const next = url.searchParams.get('next') ?? '/demo/supa'
 
-  /**
-   * Clean up the redirect URL by deleting the Auth flow parameters.
-   *
-   * `next` is preserved for now, because it's needed in the error case.
-   */
-  const redirectTo = new URL(url)
-  redirectTo.pathname = next
-  redirectTo.searchParams.delete('token_hash')
-  redirectTo.searchParams.delete('type')
+	/**
+	 * Clean up the redirect URL by deleting the Auth flow parameters.
+	 *
+	 * `next` is preserved for now, because it's needed in the error case.
+	 */
+	const redirectTo = new URL(url)
+	redirectTo.pathname = next
+	redirectTo.searchParams.delete('token_hash')
+	redirectTo.searchParams.delete('type')
 
-  if (token_hash && type) {
-    const { error } = await supabase.auth.verifyOtp({ type, token_hash })
-    if (!error) {
-      console.log('Email confirmed successfully');
-      redirectTo.searchParams.delete('next')
-      redirect(303, redirectTo)
-    }
-  }
+	if (token_hash && type) {
+		const { error, data } = await supabase.auth.verifyOtp({ type, token_hash })
+		if (!error) {
+			console.log('Email confirmed successfully');
+			console.log('Session: ', data.session);
+			redirectTo.searchParams.delete('next')
+			redirect(303, redirectTo)
+		} else {
+			console.error('Error confirming email:', error.message)
+		}
+	}
 
-  redirectTo.pathname = '/auth/error'
-  redirect(303, redirectTo)
+	redirectTo.pathname = '/auth/error'
+	redirect(303, redirectTo)
 }
