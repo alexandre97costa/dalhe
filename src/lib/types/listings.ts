7f07b81171c176347e17b275093331ebd8dc05f1
Alexandre Costa <alexandre97costa@gmail.com>
2026-02-17 11:46:08 +0000

    listing 2

--- src/lib/types/listings.ts
+++ src/lib/types/listings.ts
@@ -7,8 +7,12 @@ let laptime: Tables<'lap_time'>
 export interface RecentLaptime {
     id: Tables<'lap_time'>['id']
     laptime: `${Tables<'lap_time'>['minutes']}:${Tables<'lap_time'>['seconds']}.${Tables<'lap_time'>['sub_seconds']}`
+    track_id: Tables<'lap_time'>['track_id']
     track: Tables<'race_track'>['name']
+    car_id: Tables<'lap_time'>['car_id']
     car_make: Tables<'car_make'>['name']
     car_model: Tables<'car'>['model']
+    driver_id: Tables<'lap_time'>['driver_id']
     driver: Tables<'profiles'>['username']
+    driver_avatar: Tables<'profiles'>['avatar_url']
 }
