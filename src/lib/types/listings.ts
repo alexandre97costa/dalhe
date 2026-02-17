46fc29ff712d9b9fcb6477dc388c38b2fb8644a6
Alexandre Costa <alexandre97costa@gmail.com>
2026-02-17 10:55:37 +0000

    listing init

--- src/lib/types/listings.ts
+++ src/lib/types/listings.ts
@@ -1,4 +1,14 @@
 import type { Database, Tables, Enums } from './supabase.types'
 
 // example of how to use the types
+// https://supabase.com/docs/reference/javascript/typescript-support
 let laptime: Tables<'lap_time'>
+
+export interface RecentLaptime {
+    id: Tables<'lap_time'>['id']
+    laptime: `${Tables<'lap_time'>['minutes']}:${Tables<'lap_time'>['seconds']}.${Tables<'lap_time'>['sub_seconds']}`
+    track: Tables<'race_track'>['name']
+    car_make: Tables<'car_make'>['name']
+    car_model: Tables<'car'>['model']
+    driver: Tables<'profiles'>['username']
+}
