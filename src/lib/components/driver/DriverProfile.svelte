c522048c9e7a1d024c3a274ee6e16e8d3a7a5775
Alexandre Costa <alexandre97costa@gmail.com>
2026-04-01 00:08:07 +0100

    hjk

--- src/lib/components/driver/DriverProfile.svelte
+++ src/lib/components/driver/DriverProfile.svelte
@@ -2,22 +2,45 @@
 <!-- Quando o user for o em sessão, pode editar o nome e foto (duas ações diferentes) -->
 
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button/index';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
+    import {Pencil } from '@lucide/svelte';
 
-    import { Button } from '$lib/components/ui/button/index';
-    import * as Card from '$lib/components/ui/card/index.js';
 
-    interface Props {
-        username: string;
-        photoUrl: string;
-        isCurrentUser: boolean;
-    }
+	interface Props {
+		username: string;
+		photoUrl: string;
+		isCurrentUser: boolean;
+	}
 
-    let { username, photoUrl, isCurrentUser }: Props = $props();
+	let { username, photoUrl, isCurrentUser }: Props = $props();
 
-    const handleEditProfile = () => {
-        // Lógica para editar o perfil do driver
-        console.log('Editar perfil do driver');
-    };
-
+	const handleEditProfile = () => {
+		// Lógica para editar o perfil do driver
+		console.log('Editar perfil do driver');
+	};
 </script>
+
+
+<div class="flex gap-4">
+    <Avatar.Root class="size-20">
+        <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+        <Avatar.Fallback>CN</Avatar.Fallback>
+    </Avatar.Root>
+
+    <div class="flex-1 flex flex-col gap-2">
+
+        <div class="flex items-center justify-between gap-4">
+
+            <h2 class="text-xl font-semibold">{username}</h2>
+            {#if isCurrentUser}
+            <Button variant="outline" size="sm" onclick={handleEditProfile}>
+                <span>Editar</span> 
+                <Pencil class="size-3" />
+            </Button>
+            {/if}
+        </div>
+    </div>
+</div>
 
--- src/routes/(authorized)/driver/[slug]/+page.svelte
+++ src/routes/(authorized)/driver/[slug]/+page.svelte
@@ -3,12 +3,15 @@
 	import { m } from '$lib/paraglide/messages.js';
 	import type { RecentLaptime } from '$lib/types/listings';
 	import RecentEntry from '$lib/components/leaderboards/recent_entry.svelte';
+	import DriverProfile from '$lib/components/driver/DriverProfile.svelte';
 
 	title.set(m.nav_profile());
 	let { data } = $props();
 </script>
 
-
+<div class="pt-4 pb-6">
+	<DriverProfile username={'Zé Manel'} photoUrl={'data.driver.photoUrl'} isCurrentUser={true} />
+</div>
 
 <div class="flex flex-col gap-4">
 	{#each data.laptimes as laptime}
