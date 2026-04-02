e11d8e7e0602db7c809bf183510d68e65a820733
Alexandre Costa <alexandre97costa@gmail.com>
2026-04-02 01:02:00 +0100

    im going through changeeees

--- src/lib/components/leaderboards/recent_entry.svelte
+++ src/lib/components/leaderboards/recent_entry.svelte
@@ -2,6 +2,7 @@
 	import { m } from '$lib/paraglide/messages.js';
 	import { formatTimeSinceCreation, formatLaptime, compareLaptimes } from '$lib/utils/laptime';
 	import type { RecentLaptime } from '$lib/types/listings.js';
+	import { cn } from '$lib/utils';
 
 	let { laptime }: { laptime: RecentLaptime } = $props();
 
@@ -9,7 +10,7 @@
 </script>
 
 <div>
-	{#if laptime.is_track_record}
+	<!-- {#if laptime.is_track_record}
 		<div
 			class="-mb-2 flex w-full rounded-md rounded-b-none border border-b-0 bg-purple-500/20 px-3 pt-1 pb-3 text-xs text-purple-400"
 		>
@@ -21,29 +22,54 @@
 		>
 			{m.laptime_personal_best()}
 		</div>
-	{/if}
-	<div class="bg-card flex items-center gap-4 rounded-md border px-3 py-2">
-
-		<div class="flex flex-col flex-1">
+	{/if} -->
+	<div
+		class={cn(
+			`to-background flex items-center gap-4 rounded-md border bg-linear-to-bl px-3 py-2 `,
+			laptime.is_track_record
+				? 'from-purple-500/15 border-purple-500/10'
+				: laptime.is_personal_best
+					? 'from-yellow-500/15 border-yellow-500/10'
+					: 'from-card'
+		)}
+	>
+		<div class="flex flex-1 flex-col gap-1">
 			<p class="lh-1 text-muted-foreground/50 text-xs">
 				{formatTimeSinceCreation(laptime.created_at ?? new Date().toISOString())}
 			</p>
-			<p class="text-md font-extralight">{laptime.driver}</p>
+			<p class="text-md font-light tracking-wide">{laptime.driver}</p>
 			<p class="lh-1 text-muted-foreground text-sm">
 				<span class="text-muted-foreground/50 mr-1">{m.laptime_details_with_car()}</span>
 				{`${laptime.car_make} ${laptime.car_model}`}
 			</p>
 		</div>
-		<div class="flex flex-col justify-end text-right self-end">
-			<p
-				class={`lh-1 font-mono text-xs  ${diff == '0.0s' ? 'text-muted-foreground/50' : isFaster ? 'text-green-400/70' : 'text-red-400/70'}`}
-			>
-				{diff}
-			</p>
-			<h1 class="lh-1 text-md font-mono font-medium tracking-widest">
+		<div class="flex flex-col gap-0.5 justify-end self-end text-right">
+			{#if laptime.is_track_record}
+				<p class="lh-1 text-xs text-purple-400">
+					{m.laptime_track_record()}
+				</p>
+			{:else if laptime.is_personal_best}
+				<p class="lh-1 text-xs text-yellow-400">
+					{m.laptime_personal_best()}
+				</p>
+			{:else}
+				<p
+					class={cn(`lh-1 font-mono text-xs  
+						${
+							diff == '0.0s'
+								? 'text-muted-foreground/50'
+								: isFaster
+									? 'text-green-400/70'
+									: 'text-red-400/70'
+						}`)}
+				>
+					{diff}
+				</p>
+			{/if}
+			<h1 class="lh-1 text-xl font-mono font-medium text-shadow-md text-foreground-100/80 tracking-widest">
 				{formatLaptime(laptime.laptime)}
 			</h1>
-			<p class="lh-1 text-muted-foreground text-sm" >
+			<p class="lh-1 text-muted-foreground text-sm">
 				<span class="text-muted-foreground/50 mr-1">{m.laptime_details_at_track()}</span>
 				{laptime.track_name}
 			</p>
--- src/routes/(authorized)/driver/[slug]/+page.svelte
+++ src/routes/(authorized)/driver/[slug]/+page.svelte
@@ -3,69 +3,86 @@
 	import { m } from '$lib/paraglide/messages.js';
 	import type { RecentLaptime } from '$lib/types/listings';
 	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Accordion from '$lib/components/ui/accordion/index.js';
 	import * as Item from '$lib/components/ui/item/index.js';
 	import RecentEntry from '$lib/components/leaderboards/recent_entry.svelte';
 	import DriverProfile from '$lib/components/driver/DriverProfile.svelte';
-	import { Trophy, BicepsFlexed, Medal, Award, Tally5 } from '@lucide/svelte';
+	import {
+		type Icon as IconType,
+		Trophy,
+		BicepsFlexed,
+		Medal,
+		Award,
+		Tally5
+	} from '@lucide/svelte';
 
 	title.set(m.nav_profile());
 	let { data } = $props();
 </script>
 
-<div class="flex flex-col gap-4">
+{#snippet StatCard(title: string, value: string, icon: typeof IconType, color: string)}
+	{@const Icon = icon}
+	<Item.Root variant="outline" class="flex gap-1 bg-linear-to-bl from-card to-background">
+		<Item.Header class="flex gap-0">
+			<Item.Title>{title}</Item.Title>
+		</Item.Header>
+		<Item.ItemContent class="flex-1 flex-row gap-1">
+			<Icon class={`size-6 ${color}`} />
+			<Item.Description class={`text-xl font-light`}>{value}</Item.Description>
+		</Item.ItemContent>
+	</Item.Root>
+{/snippet}
+
+{#snippet AccordionTitle(title: string)}
+	<Accordion.Trigger class="items-center">
+		<h1 class="text-foreground text-xl font-bold tracking-normal text-balance">{title}</h1>
+	</Accordion.Trigger>
+{/snippet}
+
+<div class="flex flex-col">
 	<DriverProfile username={'Alexandre'} photoUrl={'data.driver.photoUrl'} isCurrentUser={true} />
-	<Separator />
 
-	<div class="grid grid-cols-2 gap-4">
-		<Item.Root variant="outline" class="flex gap-1">
-			<Item.Header class="flex gap-0">
-				<Item.Title>Podiums</Item.Title>
-			</Item.Header>
-			<Item.ItemContent class="flex-1 flex-row gap-1">
-				<Award class="size-6 text-rose-500" />	
-				<Item.Description class="text-xl font-light">53</Item.Description>
-			</Item.ItemContent>
-		</Item.Root>
-		<Item.Root variant="outline" class="flex gap-1">
-			<Item.Header class="flex gap-0">
-				<Item.Title>Poles</Item.Title>
-			</Item.Header>
-			<Item.ItemContent class="flex-1 flex-row gap-1">
-				<Medal class="size-6 text-amber-500" />	
-				<Item.Description class="text-xl font-light">32</Item.Description>
-			</Item.ItemContent>
-		</Item.Root>
-		<Item.Root variant="outline" class="flex gap-1">
-			<Item.Header class="flex gap-0">
-				<Item.Title>Dominance</Item.Title>
-			</Item.Header>
-			<Item.ItemContent class="flex-1 flex-row gap-1">
-				<BicepsFlexed class="size-6 text-purple-500" />	
-				<Item.Description class="text-xl font-light">32 pts</Item.Description>
-			</Item.ItemContent>
-		</Item.Root>
-		<Item.Root variant="outline" class="flex gap-1">
-			<Item.Header class="flex gap-0">
-				<Item.Title>Laps submitted</Item.Title>
-			</Item.Header>
-			<Item.ItemContent class="flex-1 flex-row gap-1">
-				<Tally5 class="size-6 text-blue-500" />	
-				<Item.Description class="text-xl font-light">257</Item.Description>
-			</Item.ItemContent>
-		</Item.Root>
-	</div>
+	<Separator class="mt-4" />
 
-	<Separator />
-	{#each data.laptimes as laptime}
-		<RecentEntry {laptime} />
-	{:else}
-		<div class="flex flex-col items-center justify-center py-40">
-			<span class="text-foreground text-lg font-medium tracking-tight">
-				{m.home_no_laptimes()}
-			</span>
-			<span class="text-muted-foreground text-sm">
-				{m.home_no_laptimes_description()}
-			</span>
-		</div>
-	{/each}
+	<Accordion.Root type="multiple" value={['item-1', 'item-2', 'item-3']} class="w-full">
+		<!-- Stats Accordion -->
+		<Accordion.Item value="item-1">
+			{@render AccordionTitle('Stats')}
+			<Accordion.Content class="pb-4">
+				<div class="grid grid-cols-2 gap-4 md:grid-cols-4">
+					{@render StatCard('Podiums', '53', Award, 'text-rose-500')}
+					{@render StatCard('Poles', '32', Medal, 'text-amber-500')}
+					{@render StatCard('Dominance', '32 pts', BicepsFlexed, 'text-purple-500')}
+					{@render StatCard('Laps submitted', '257', Tally5, 'text-blue-500')}
+				</div>
+			</Accordion.Content>
+		</Accordion.Item>
+
+		<!-- Progress Accordion -->
+		<Accordion.Item value="item-2">
+			{@render AccordionTitle('Progress')}
+			<Accordion.Content class="pb-4 text-muted-foreground">Work in progress</Accordion.Content>
+		</Accordion.Item>
+
+		<!-- Recent Laptimes Accordion -->
+		<Accordion.Item value="item-3">
+			{@render AccordionTitle('Recent Laptimes')}
+			<Accordion.Content class="pb-4">
+				<div class="flex flex-col gap-4">
+					{#each data.laptimes as laptime}
+						<RecentEntry {laptime} />
+					{:else}
+						<div class="flex flex-col items-center justify-center py-40">
+							<span class="text-foreground text-lg font-medium tracking-tight">
+								{m.home_no_laptimes()}
+							</span>
+							<span class="text-muted-foreground text-sm">
+								{m.home_no_laptimes_description()}
+							</span>
+						</div>
+					{/each}
+				</div>
+			</Accordion.Content>
+		</Accordion.Item>
+	</Accordion.Root>
 </div>
